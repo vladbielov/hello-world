a9b5a13d9f2483bdb32d8e5b57176d10f22edd18
vladbielov <bestyugan@gmail.com>
2020-12-27 13:08:55 +0000

    ready to build

--- deployments/terraform/variables.tf
+++ deployments/terraform/variables.tf
@@ -1,19 +1,45 @@
-variable  "deployment_image" {
-    default = "docker.fuchicorp.com/hello-world-app-dev-feature:4b7aacb"
-}
 variable "deployment_environment" {
-    default = "dev"
+    default     = "dev"
+    description = "- (Optional) The namespace to deploy the helm chart."
 }
+
+variable "deployment_name" {
+    default     = "hello-world"
+    description = "- (Optional) The name of the deployment."
+}
+
+variable "deployment_image" {
+    default     = "fsadykov/hello-world:latest"
+    description = "- (Optional) The name of the deployment."
+} 
+
 variable "deployment_endpoint" {
     type = "map"
-     default = {
-        test  = "test.hello"
-        dev  = "dev.hello"
-        qa   = "qa.hello"
-        prod = "hello"
-        stage = "stage.hello"
-  }
+    default = {
+        dev     = "dev.hello.tridentfe.com"
+        qa      = "qa.hello.tridentfe.com"
+        prod    = "hello.tridentfe.com"
+        stage   = "stage.hello.tridentfe.com"
+    }
+    description = "- (Optional) The name of the deployment."
 }
-variable "google_domain_name" {
-    default = "fuchicorp.com"
-}
+
+# variable  "deployment_image" {
+#     default = "docker.fuchicorp.com/hello-world-app-dev-feature:4b7aacb"
+# }
+# variable "deployment_environment" {
+#     default = "dev"
+# }
+# variable "deployment_endpoint" {
+#     type = "map"
+#      default = {
+#         test  = "test.hello"
+#         dev  = "dev.hello"
+#         qa   = "qa.hello"
+#         prod = "hello"
+#         stage = "stage.hello"
+#   }
+# }
+# variable "google_domain_name" {
+#     default = "fuchicorp.com"
+# }
